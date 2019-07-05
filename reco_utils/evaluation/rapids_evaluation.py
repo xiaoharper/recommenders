bc5174bea6ebfebb9b5e3c1572fc3829d6855d85
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2019-07-05 19:31:48 +0000

    Add RMSE

---
+++ reco_utils/evaluation/rapids_evaluation.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+from reco_utils.common.constants import (
+    DEFAULT_USER_COL,
+    DEFAULT_ITEM_COL,
+    DEFAULT_RATING_COL,
+    DEFAULT_PREDICTION_COL,
+    DEFAULT_K,
+    DEFAULT_THRESHOLD,
+)
+
+
+def merge_rating_true_pred(
+    rating_true,
+    rating_pred,
+    col_user=DEFAULT_USER_COL,
+    col_item=DEFAULT_ITEM_COL,
+    col_rating=DEFAULT_RATING_COL,
+    col_prediction=DEFAULT_PREDICTION_COL,
+):
+    """Join truth and prediction data frames on userID and itemID and return the true
+    and predicted rated with the correct index.
+    
+    Args:
+        rating_true (pd.DataFrame): True data
+        rating_pred (pd.DataFrame): Predicted data
+        col_user (str): column name for user
+        col_item (str): column name for item
+        col_rating (str): column name for rating
+        col_prediction (str): column name for prediction
+
+    Returns:
+        np.array: Array with the true ratings
+        np.array: Array with the predicted ratings
+
+    """
+
+    # pd.merge will apply suffixes to columns which have the same name across both dataframes
+    suffixes = ["_true", "_pred"]
+    rating_true_pred = rating_true.merge(
+        rating_pred, on=[col_user, col_item], suffixes=suffixes
+    )
+    if col_rating in rating_pred.columns:
+        col_rating = col_rating + suffixes[0]
+    if col_prediction in rating_true.columns:
+        col_prediction = col_prediction + suffixes[1]
+    return rating_true_pred[col_rating], rating_true_pred[col_prediction]
+
+
+def rmse(
+    rating_true,
+    rating_pred,
+    col_user=DEFAULT_USER_COL,
+    col_item=DEFAULT_ITEM_COL,
+    col_rating=DEFAULT_RATING_COL,
+    col_prediction=DEFAULT_PREDICTION_COL,
+):
+    y_true, y_pred = merge_rating_true_pred(
+        rating_true=rating_true,
+        rating_pred=rating_pred,
+        col_user=col_user,
+        col_item=col_item,
+        col_rating=col_rating,
+        col_prediction=col_prediction,
+    )
+    squared_error = (y_true - y_pred)**2
+    return np.sqrt(squared_error.mean())
---
+++ tests/unit/test_rapids_evaluation.py
@@ -0,0 +1,123 @@
+import cudf as cu
+import numpy as np
+
+from reco_utils.common.constants import (
+    DEFAULT_USER_COL,
+    DEFAULT_ITEM_COL,
+    DEFAULT_RATING_COL,
+    DEFAULT_PREDICTION_COL,
+    DEFAULT_K,
+    DEFAULT_THRESHOLD,
+)
+
+TOL = 0.0001
+
+
+@pytest.fixture
+def rating_true():
+    return cu.DataFrame(
+        [
+            (DEFAULT_USER_COL, [1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]),
+            (DEFAULT_ITEM_COL, [
+                1,
+                2,
+                3,
+                1,
+                4,
+                5,
+                6,
+                7,
+                2,
+                5,
+                6,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+            ]),
+            (DEFAULT_RATING_COL, [5, 4, 3, 5, 5, 3, 3, 1, 5, 5, 5, 4, 4, 3, 3, 3, 2, 1]),
+        ]
+    )
+
+
+@pytest.fixture
+def rating_pred():
+    return cu.DataFrame(
+        [
+            (DEFAULT_USER_COL, [1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]),
+            (DEFAULT_ITEM_COL, [
+                3,
+                10,
+                12,
+                10,
+                3,
+                5,
+                11,
+                13,
+                4,
+                10,
+                7,
+                13,
+                1,
+                3,
+                5,
+                2,
+                11,
+                14,
+            ]),
+            (DEFAULT_PREDICTION_COL, [
+                14,
+                13,
+                12,
+                14,
+                13,
+                12,
+                11,
+                10,
+                14,
+                13,
+                12,
+                11,
+                10,
+                9,
+                8,
+                7,
+                6,
+                5,
+            ]),
+            (DEFAULT_RATING_COL, [5, 4, 3, 5, 5, 3, 3, 1, 5, 5, 5, 4, 4, 3, 3, 3, 2, 1]),
+        ]
+    )
+
+
+def test_merge_rating(rating_true, rating_pred):
+    y_true, y_pred = merge_rating_true_pred(
+        rating_true,
+        rating_pred,
+        col_user=DEFAULT_USER_COL,
+        col_item=DEFAULT_ITEM_COL,
+        col_rating=DEFAULT_RATING_COL,
+        col_prediction=DEFAULT_PREDICTION_COL,
+    )
+#   To convert into np.array, y_true.to_array()
+    target_y_true = np.array([3, 3, 5, 5, 3, 3, 2, 1])
+    target_y_pred = np.array([14, 12, 7, 8, 13, 6, 11, 5])
+
+    assert y_true.shape == y_pred.shape
+    assert np.all(y_true.to_array() == target_y_true)
+    assert np.all(y_pred.to_array() == target_y_pred)
+
+    
+def test_rapids_rmse(rating_true, rating_pred):
+    assert (
+        rmse(
+            rating_true=rating_true,
+            rating_pred=rating_true,
+            col_prediction=DEFAULT_RATING_COL,
+        )
+        == 0
+    )
+    assert rmse(rating_true, rating_pred) == pytest.approx(7.254309, TOL)
